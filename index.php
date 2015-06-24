926c2f1bc72005f229bb12194db3a4c4c6206487
Max Cantu <max@auralabs.co>
2015-06-24 17:13:28 -0500

    size

--- index.php
+++ index.php
@@ -34,18 +34,22 @@
 			section > special
 		</section>
 		
-		<section>
+		<section id="posts">
 			<div class="post">
 				<h2 class="vox-post-title vox-color-entretenimiento">El secreto de Adaline, excelente para ver con tu pareja</h2>
+				<div class="voz-post-image" style="background-image: url('http://placehold.it/250x250?text=imagen');"></div>
 			</div>
 			<div class="post">
 				<h2 class="vox-post-title vox-color-ciencia-y-tecnologia">Gula tecnológica</h2>
+				<div class="voz-post-image" style="background-image: url('http://placehold.it/250x250?text=imagen');"></div>
 			</div>
 			<div class="post">
 				<h2 class="vox-post-title vox-color-mundo">Se unen líderes religiosos para defender a familias migrantes</h2>
+				<div class="voz-post-image" style="background-image: url('http://placehold.it/250x250?text=imagen');"></div>
 			</div>
 			<div class="post">
 				<h2 class="vox-post-title vox-color-cultura">La Torre Eiffel cumple 126 años</h2>
+				<div class="voz-post-image" style="background-image: url('http://placehold.it/250x250?text=imagen');"></div>
 			</div>
 		</section>
 	</div>
