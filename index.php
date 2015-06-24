dcf02f6785d560ab4fd2c30b58db4ee2c645fb6c
Max Cantu <max@auralabs.co>
2015-06-24 16:27:36 -0500

    html5

--- index.php
+++ index.php
@@ -21,8 +21,27 @@
     <![endif]-->
   </head>
   <body>
-    Hello
-
+    <div class="container">
+		<header>
+			header
+		</header>
+		
+		<nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">About</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+		
+		<section>
+			section > special
+		</section>
+		
+		<section>
+			section > posts
+		</section>
+	</div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
