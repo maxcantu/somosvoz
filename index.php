8610473d81738540a0a508d65664d86e75bc0669
Max Cantu <max@auralabs.co>
2015-06-24 16:28:41 -0500

    sections

--- index.php
+++ index.php
@@ -27,11 +27,7 @@
 		</header>
 		
 		<nav>
-          <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li>
-          </ul>
+			nav
         </nav>
 		
 		<section>
