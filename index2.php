8e3915d842c22ad2595c28b2a128f6c5c5ae3219
Max Cantu <max@auralabs.co>
2015-06-24 20:26:03 -0500

    infinte scroll

--- index2.php
+++ index2.php
@@ -1,43 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>SomosVoz</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    
-    <!-- CSS -->
-    <link href="css/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-	<!-- This is the overlay DIV that is displayed while loading a new page -->
-	<div id="loader"></div> 
-	
-    <div class="container">
-		<header>
-			header
-		</header>
-		
-		<nav>
-			nav
-        </nav>
-		
-		<section>
-			section > special
-		</section>
-		
-		<section id="scroll-container">
 			<div class="post">
 				<h2 class="vox-post-title vox-color-entretenimiento">El secreto de Adaline, excelente para ver con tu pareja</h2>
 				<div class="voz-post-image" style="background-image: url('http://placehold.it/250x250?text=imagen');"></div>
@@ -86,21 +46,3 @@
 				<h2 class="vox-post-title vox-color-cultura">La Torre Eiffel cumple 126 años</h2>
 				<div class="voz-post-image" style="background-image: url('http://placehold.it/250x250?text=imagen');"></div>
 			</div>
-		</section>
-	</div>
-	
-	<!-- These are all the links that can be loaded. -->
-	<div id="pages">
-		<a href="./index.php"></a>
-		<a href="./index2.php"></a>
-		<a href="./index3.php"></a>
-	</div>
-	
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- JS -->
-    <script src="js/script.js"></script>
-  </body>
-</html>
