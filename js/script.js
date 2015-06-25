951b343a45585aa15367d50a3cb0b873c0a067ed
Max Cantu <max@auralabs.co>
2015-06-24 21:18:03 -0500

    isotope

--- js/script.js
+++ js/script.js
@@ -44,6 +44,12 @@ jQuery(document).ready(function() {
 					$('#loader').fadeOut('slow', function() {
 						$('#scroll-container').append(data).fadeIn(999);
 						current=position;
+						// Isotope JS
+						$('#scroll-container').isotope({
+						  // options
+						  itemSelector: '.post',
+						  layoutMode: 'fitRows'
+						});
 					});
 				});
 			});
